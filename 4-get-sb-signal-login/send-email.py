adf6c34ae326c7f8ecab47aaebafb891edc41b46
sika <simon.noworkemail@gmail.com>
2018-03-17 17:49:43 +0100

    misc changes to send-email.py

--- 4-get-sb-signal-login/send-email.py
+++ 4-get-sb-signal-login/send-email.py
@@ -24,22 +24,13 @@ def getCredentials(domain):
     except Exception as e:
         print ("ERROR in", inspect.stack()[0][3], ':', str(e))
 
-def send_mail(send_from, send_to, subject, text, files):
-    # files=None
+def send_mail(send_to, subject, text, files):
     try:
-        # BP()
-        # assert isinstance(send_to, list)
-
         msg = MIMEMultipart()
-        msg['From'] = send_from
-        msg['To'] = COMMASPACE.join(send_to)
-        # msg['Date'] = formatdate(localtime=True)
         msg['Subject'] = subject
 
         msg.attach(MIMEText(text))
 
-        # file_orderStat = pathFile + sPathOutput + 'confirmationStatistics.csv'
-        # file_exists = os.path.isfile(file_orderStat)
         for file in files:
             with open (file, 'rb') as csvFile:
                 part = MIMEApplication(csvFile.read(),Name=os.path.basename(file))
@@ -50,15 +41,14 @@ def send_mail(send_from, send_to, subject, text, files):
         smtp.starttls()
         credGmailAutotrading = getCredentials(gloCredGmailAutotrading)
         smtp.login(credGmailAutotrading.get('username'), credGmailAutotrading.get('pwd'))
-        smtp.sendmail(send_from, send_to, msg.as_string())
+        smtp.sendmail(credGmailAutotrading.get('username'), send_to, msg.as_string())
         smtp.close()
 
     except Exception as e:
         print ("ERROR in", inspect.stack()[0][3], ':', str(e)) 
 
-
 files= [
 pathFile + sPathOutput + 'confirmationStatistics.csv',
 pathFile + sPathOutput + 'orderStatistics.csv'
 ]
-send_mail('simon.autotrading@gmail.com', 'simon.autotrading@gmail.com', 'subj', 'text', files)
+send_mail('simon.autotrading@gmail.com', 'autotrade files', '', files)
